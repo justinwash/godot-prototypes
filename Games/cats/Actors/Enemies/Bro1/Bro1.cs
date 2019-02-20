6974525e755f3d24343dbd4cf95424fad325df36
justin wash <justin.wash@pm.me>
2019-02-19 21:57:51 -0600

    health meter (kinda)

--- Games/cats/Actors/Enemies/Bro1/Bro1.cs
+++ Games/cats/Actors/Enemies/Bro1/Bro1.cs
@@ -4,13 +4,13 @@ using System;
 public class Bro1 : Area2D
 {
 	private Area2D _player;
-	private Polygon2D _playerHealthBar;
+	private Node2D _playerHealthBar;
   	private Sprite _sprite;
 	private AnimationPlayer _animationPlayer;
 	
   	public override void _Ready() {
 		_player = GetNode<Area2D>("../Player");
-		_playerHealthBar = GetNode<Polygon2D>("../Player/Camera2D/MoodMeter/Meter");
+		_playerHealthBar = GetNode<Node2D>("../UI/MoodMeter/Meter");
     	_sprite = GetNode<Sprite>("Sprite");
 		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
   	}
@@ -23,8 +23,8 @@ public class Bro1 : Area2D
       		_animationPlayer.Play("idle_right");
 		}
 		
-		if (Position.x - 100 - _player.Position.x < 100) {
-			//_playerHealthBar.Scale(null);
+		if (Position.x - _player.Position.x < 100) {
+			_playerHealthBar.SetScale(_playerHealthBar.Scale * new Vector2(1f,0.95f));
 		}
 	}
 }
