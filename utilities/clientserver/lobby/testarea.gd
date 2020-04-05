8ab24d43ea4f6393030ef2b8452954636ecfdcf8
Justin Wash <justin.wash@pm.me>
2020-04-05 12:49:07 -0500

    fix breakage

--- utilities/clientserver/lobby/testarea.gd
+++ utilities/clientserver/lobby/testarea.gd
@@ -1,17 +1,25 @@
 extends Node2D
 
+var player1
+var player2
+
 func _ready():
-	$TestMover2.speed = 0
 	get_tree().connect("network_peer_connected", self, "_player_connected")
 	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
 		
 func _player_connected(_id):
+	player1 = preload('res://mover/mover.tscn').instance()
+	player1.position = $SpawnPoints/Spawn.position
+	player2 = preload('res://mover/mover.tscn').instance()
+	player2.position = $SpawnPoints/Spawn2.position
+	
+	
 	if get_tree().is_network_server():
-		$TestMover2.set_network_master(get_tree().get_network_connected_peers()[0])
-		$TestMover2.speed = 200
-	else:
-		$TestMover2.set_network_master(get_tree().get_network_unique_id())
+		player2.set_network_master(get_tree().get_network_connected_peers()[0])
 		
+	add_child(player1)
+	add_child(player2)
+	
 	print("my unique id: ", get_tree().get_network_unique_id())
 		
 func _player_disconnected(_id):
