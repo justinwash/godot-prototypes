c4275a119b7883120b93b871709f468550b600c8
Justin Wash <justin.wash@pm.me>
2020-04-05 11:18:34 -0500

    "text changes"

--- utilities/clientserver/lobby/PlayerPanel.gd
+++ utilities/clientserver/lobby/PlayerPanel.gd
@@ -7,7 +7,7 @@ func _ready():
 	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
 
 func _player_connected(id):
-	$Player2Label.text = "Player 2: " + str(id)
+	$Player2Label.text = "" + str(id) if id != 1 else "Host"
 	
 func _player_disconnected(id):
 	$Player2Label.text = ""
