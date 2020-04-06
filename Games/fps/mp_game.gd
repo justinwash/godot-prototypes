8ca450145fd1e0a68018ade50051e5b5587e5be4
Justin Wash <justin.wash@pm.me>
2020-04-05 23:12:00 -0500

    player2 netwrorking init

---
+++ Games/fps/mp_game.gd
@@ -0,0 +1,24 @@
+extends Node
+
+onready var player1 = $Player1
+onready var player2 = $Player2
+
+func _ready():
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+	get_tree().connect("server_disconnected", self, "_server_disconnected")
+		
+func _player_connected(_id):
+	if get_tree().has_network_peer() and get_tree().is_network_server():
+		player2.set_network_master(get_tree().get_network_connected_peers()[0])
+		
+	elif get_tree().has_network_peer():
+		player2.set_network_master(get_tree().get_network_unique_id())
+	
+	print("my unique id: ", get_tree().get_network_unique_id())
+		
+#func _player_disconnected(_id):
+#	player2.queue_free()
+#
+#func _server_disconnected(_id):
+#	player1.queue_free()
--- Games/fps/player/Player.gd
+++ Games/fps/player/Player.gd
@@ -7,40 +7,37 @@ onready var raycast = $RayCast
 onready var anim = $AnimationPlayer
 
 func _ready():
+	if get_tree().has_network_peer() and is_network_master():
+		$Camera.current = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	yield(get_tree(), "idle_frame")
-	get_tree().call_group("targets", "set_player", self)
 
 func _input(event):
-	if event is InputEventMouseMotion:
-		rotation_degrees.y -= MOUSE_SENS * event.relative.x
-		rotation_degrees.x -= MOUSE_SENS * event.relative.y
-
-func _process(_delta):
-	if Input.is_action_pressed("exit"):
-		get_tree().quit()
-	if Input.is_action_pressed("restart"):
-		kill()
+	if get_tree().has_network_peer() and is_network_master():
+		if event is InputEventMouseMotion:
+			rotation_degrees.y -= MOUSE_SENS * event.relative.x
+			rotation_degrees.x -= MOUSE_SENS * event.relative.y
 
 func _physics_process(delta):
-	var move_vec = Vector3()
-	if Input.is_action_pressed("move_forwards"):
-		move_vec.z -= 1
-	if Input.is_action_pressed("move_backwards"):
-		move_vec.z += 1
-	if Input.is_action_pressed("move_left"):
-		move_vec.x -= 1
-	if Input.is_action_pressed("move_right"):
-		move_vec.x += 1
-	move_vec = move_vec.normalized()
-	move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
-	move_and_collide(move_vec * MOVE_SPEED * delta)
+	if get_tree().has_network_peer() and is_network_master():
+		$Camera.current = true
+		var move_vec = Vector3()
+		if Input.is_action_pressed("move_forwards"):
+			move_vec.z -= 1
+		if Input.is_action_pressed("move_backwards"):
+			move_vec.z += 1
+		if Input.is_action_pressed("move_left"):
+			move_vec.x -= 1
+		if Input.is_action_pressed("move_right"):
+			move_vec.x += 1
+		move_vec = move_vec.normalized()
+		move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
+		move_and_collide(move_vec * MOVE_SPEED * delta)
 	
-	if Input.is_action_pressed("shoot"):
-		anim.play("shoot")
-		var coll = raycast.get_collider()
-		if raycast.is_colliding() and coll.has_method("kill"):
-			coll.kill()
+		if Input.is_action_pressed("shoot"):
+			anim.play("shoot")
+			var coll = raycast.get_collider()
+			if raycast.is_colliding() and coll.has_method("kill"):
+				coll.kill()
 
 func kill():
 	get_tree().reload_current_scene()
