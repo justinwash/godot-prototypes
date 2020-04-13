12189d5d8ba44ad098a2087dc1b442b4143ce3fa
Justin Wash <justin.wash@pm.me>
2020-04-13 18:13:11 -0500

    remake player controller

--- Games/fps/mp_game.gd
+++ Games/fps/mp_game.gd
@@ -9,10 +9,10 @@ var player2
 
 func _ready():
 	var _player_connected = get_tree().connect("network_peer_connected", self, "_player_connected")
-	var _player_disconnected = get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
-	var _server_disconnected = get_tree().connect("server_disconnected", self, "_server_disconnected")
+	var _player_disconnected = get_tree().connect("network_peer_disconnected", self, "_end_game")
+	var _server_disconnected = get_tree().connect("server_disconnected", self, "_end_game")
 	var _matching_started = lobby.get_node("MatchMakingPanel").connect("matching_started", self, "_matching_started")
-	var _matching_canceled = lobby.get_node("MatchMakingPanel").connect("matching_canceled", self, "_matching_canceled")
+	var _matching_canceled = lobby.get_node("MatchMakingPanel").connect("matching_canceled", self, "_end_game")
 		
 func _matching_started():
 	# start waiting/training mode
@@ -24,32 +24,25 @@ func _matching_canceled():
 
 func _player_connected(_id):
 	player1 = preload('res://player/player.tscn').instance()
-	player1.translation = $Spawnpoints/Spawnpoint.translation
-	player1.rotation = $Spawnpoints/Spawnpoint.rotation
+	player1.spawn.translation = $Spawnpoints/Spawnpoint.translation
+	player1.spawn.rotation = $Spawnpoints/Spawnpoint.rotation
 	players.add_child(player1)
 	
 	player2 = preload('res://player/player.tscn').instance()
-	player2.translation = $Spawnpoints/Spawnpoint2.translation
-	player2.rotation = $Spawnpoints/Spawnpoint2.rotation
+	player2.spawn.translation = $Spawnpoints/Spawnpoint2.translation
+	player2.spawn.rotation = $Spawnpoints/Spawnpoint2.rotation
 	if get_tree().has_network_peer() and get_tree().is_network_server():
 		player2.set_network_master(_id)
 	elif get_tree().has_network_peer():
 		player2.set_network_master(get_tree().get_network_unique_id())
 	players.add_child(player2)
-	
-	for player in players.get_children():
-		if player.get_network_master() == get_tree().get_network_unique_id():
-			player.camera.current = true
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
 
-func _player_disconnected(_id):
+func _end_game():
 	for player in players.get_children():
 		player.queue_free()
-
-func _server_disconnected(_id):
-	for player in players.get_children():
-		player.queue_free()
+	
+	lobby.visible = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
 	
 	
--- Games/fps/player/Player.gd
+++ Games/fps/player/Player.gd
@@ -1,66 +1,27 @@
-extends KinematicBody
+extends Spatial
 
-const MOVE_SPEED = 8
-const MOUSE_SENS = 0.075
+onready var status = $Status
 
-onready var raycast = $RayCast
-onready var anim = $AnimationPlayer
-onready var camera = $Camera
-onready var HUD = $HUD
-
-var spawn_translation
-var spawn_rotation
-
-var kills = 0
+var spawn = {
+	translation: Vector3(0,0,0),
+	rotation: Vector3(0,0,0)
+}
 
 func _ready():
-	spawn_translation = translation
-	spawn_rotation = rotation
+	var new_mover = preload('res://player/mover.tscn').instance()
+	new_mover.translation = spawn.translation
+	new_mover.rotation = spawn.rotation
+	$Mover.replace_by(new_mover)
 	
-	if get_tree().has_network_peer() and is_network_master():
-		for hud_element in $HUD.get_children():
-			hud_element.visible = true
-	else:
-		for hud_element in $HUD.get_children():
-			hud_element.visible = false
-
-func _input(event):
-	if get_tree().has_network_peer() and is_network_master():
-		if event is InputEventMouseMotion:
-			rotation_degrees.y -= MOUSE_SENS * event.relative.x
-			rotation_degrees.x -= MOUSE_SENS * event.relative.y
-
-func _physics_process(delta):
-	if get_tree().has_network_peer() and is_network_master():
-		$Camera.current = true
-		var move_vec = Vector3()
-		if Input.is_action_pressed("move_forwards"):
-			move_vec.z -= 1
-		if Input.is_action_pressed("move_backwards"):
-			move_vec.z += 1
-		if Input.is_action_pressed("move_left"):
-			move_vec.x -= 1
-		if Input.is_action_pressed("move_right"):
-			move_vec.x += 1
-		move_vec = move_vec.normalized()
-		move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
-		var _velocity = move_and_collide(move_vec * MOVE_SPEED * delta)
+	if get_network_master() == get_tree().get_network_unique_id():
+			$Mover.camera.current = true
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
-		if Input.is_action_pressed("shoot"):
-			anim.play("shoot")
-			var coll = raycast.get_collider()
-			if raycast.is_colliding() and coll.has_method("kill"):
-				rpc("kill", coll.get_network_master())
-				kills += 1
-		
-		if get_tree().has_network_peer():
-			rpc_unreliable("set_pos", global_transform)
-		
-puppet func set_pos(p_pos):
-	global_transform = p_pos
-
-puppet func kill(id):
-	print("got kill order for ", id)
-	if get_tree().get_network_unique_id() == id:
-		translation = spawn_translation
-		rotation = spawn_rotation
+func _respawn():
+	var new_mover = preload('res://player/mover.tscn').instance()
+	new_mover.translation = spawn.translation
+	new_mover.rotation = spawn.rotation
+	$Mover.replace_by(new_mover)
+	if get_network_master() == get_tree().get_network_unique_id():
+			$Mover.camera.current = true
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
