b251f7c15b22a5026e9667942f3fa968a310077c
Justin Wash <justin.wash@pm.me>
2020-04-12 21:38:30 -0500

    rework Player class

--- Games/fps/lobby/lobby.gd
+++ Games/fps/lobby/lobby.gd
@@ -5,7 +5,7 @@ export var matchmaking = true
 
 func _process(_delta):
 	if Input.is_action_just_pressed("pause"):
-		get_tree().paused = false if online else !get_tree().paused
+		get_tree().paused = !get_tree().paused
 		visible = !visible
 		if visible:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)  
--- Games/fps/mp_game.gd
+++ Games/fps/mp_game.gd
@@ -13,24 +13,33 @@ func _ready():
 	var _matching_canceled = lobby.get_node("MatchMakingPanel").connect("matching_canceled", self, "_matching_canceled")
 		
 func _matching_started():
-	for player in $Players.get_children():
-		player.queue_free()
-		
-	spawn_local_player()
-	lobby.visible = false
+	# start waiting/training mode
+	pass
 
 func _matching_canceled():
-	for player in $Players.get_children():
-		player.queue_free()
+	# end waiting/training mode
+	pass
 	
 func _player_connected(_id):
 	print('player connected: ', _id)
-	spawn_remote_player(_id)
+	players.append(Player.new(_id))
 	
-	for player in $Players.get_children():
-		if player.net_id == 0:
-			player.set_network_master(get_tree().get_network_unique_id())
-			player.get_node("Camera").current = true
+	var local_exists = false
+	for player in players:
+		if player.id == get_tree().get_network_unique_id():
+			local_exists = true
+	if !local_exists:
+		players.append(Player.new(get_tree().get_network_unique_id()))
+			
+#	for player in $Players.get_children():
+#		player.queue_free()
+#	spawn_remote_player(_id)
+#	spawn_local_player()
+#
+#	for player in $Players.get_children():
+#		if player.net_id == 0:
+#			player.set_network_master(get_tree().get_network_unique_id())
+#			player.get_node("Camera").current = true
 	
 #	if get_tree().has_network_peer() and get_tree().is_network_server():
 #		var new_player = preload("res://player/Player.tscn").instance()
@@ -50,12 +59,28 @@ func spawn_local_player():
 	var new_player = preload("res://player/Player.tscn").instance()
 	new_player.translation = $Spawnpoints/Spawnpoint2.translation
 	new_player.set_network_master(get_tree().get_network_unique_id())
-	new_player.net_id = 0
 	$Players.add_child(new_player)
 	
 func spawn_remote_player(id):
 	var new_player = preload("res://player/Player.tscn").instance()
 	new_player.set_network_master(id)
 	new_player.translation = $Spawnpoints/Spawnpoint.translation
-	new_player.net_id = id
 	$Players.add_child(new_player)
+	
+class Player extends Node:
+	var id
+	var node
+	
+	func _init(id):
+		id = id
+		node = create_node(id)
+	
+	func create_node(id):
+		var new_player = preload("res://player/Player.tscn").instance()
+		new_player.set_network_master(id)
+		$Players.add_child(new_player)
+		return new_player
+	
+	
+	
+	
