64934eef7a54c8d456c74599d9217b94de2ebf43
Justin Wash <justin.wash@pm.me>
2020-04-13 22:41:45 -0500

    add new fpsmulti

---
+++ Games/fpsmulti/game.gd
@@ -0,0 +1,3 @@
+extends Spatial
+
+export var online = false
---
+++ Games/fpsmulti/player/Player.gd
@@ -0,0 +1,45 @@
+extends KinematicBody
+
+const MOVE_SPEED = 8
+const MOUSE_SENS = 0.075
+
+onready var raycast = $RayCast
+onready var anim = $AnimationPlayer
+onready var camera = $Camera
+
+func _input(event):
+	if get_tree().has_network_peer() and is_network_master() or !get_tree().has_network_peer():
+		if event is InputEventMouseMotion:
+			rotation_degrees.y -= MOUSE_SENS * event.relative.x
+			rotation_degrees.x -= MOUSE_SENS * event.relative.y
+
+func _physics_process(delta):
+	if get_tree().has_network_peer() and is_network_master() or !get_tree().has_network_peer():
+		$Camera.current = true
+		var move_vec = Vector3()
+		if Input.is_action_pressed("move_forwards"):
+			move_vec.z -= 1
+		if Input.is_action_pressed("move_backwards"):
+			move_vec.z += 1
+		if Input.is_action_pressed("move_left"):
+			move_vec.x -= 1
+		if Input.is_action_pressed("move_right"):
+			move_vec.x += 1
+		move_vec = move_vec.normalized()
+		move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
+		var _velocity = move_and_collide(move_vec * MOVE_SPEED * delta)
+	
+		if Input.is_action_pressed("shoot"):
+			anim.play("shoot")
+			var coll = raycast.get_collider()
+			if raycast.is_colliding() and coll.has_method("kill"):
+				coll.kill()
+		
+		if get_tree().has_network_peer():
+			rpc_unreliable("set_pos", global_transform)
+		
+puppet func set_pos(p_pos):
+	global_transform = p_pos
+
+func kill():
+	var _new_scene = get_tree().reload_current_scene()
